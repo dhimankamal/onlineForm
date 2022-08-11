62f2fdfb8dcf365f5903e1bc9272b72a798394d8
kamal <dhimank079@gmail.com>
2022-08-11 13:25:08 +0530

    test 2

--- pages/profile.js
+++ pages/profile.js
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
-export default function Profile() {
+export default function Profile( ) {
   return (
     <>
       <Navbar transparent />
